e1edb1e0ce377c05d558bbb5e5e61099199a1aa5
Harrmalik <harrmalik@gmail.com>
2016-01-12 18:43:43 -0500

    getting heroku to work

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var express = require('express'),
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + "/views");
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,5 +19,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', routes);
 
-app.listen(8080);
-console.log("Express server started on port 8080");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
